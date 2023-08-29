a05e54f425a3cef1762198d6254d6d79f50cc769
Lolymoly <teetat.wis@gmail.com>
2023-08-29 20:29:44 +0700

    add tailwind + fix styling

--- components/Banner/page.tsx
+++ components/Banner/page.tsx
@@ -12,14 +12,20 @@ export default function Banner() {
 				/>
 			</div>
 			<div className={styles.bannerText}>
-				<h1>
+				<h1 className='font-bold text-[18px] sm:text-[36px] lg:text-[3.5vw]'>
 					จองวัคซีน COVID-19
 					<br />
 					ได้แล้ววันนี้!
 				</h1>
 				<div className={styles.bannerButtonDiv}>
-					<button className={styles.bannerButton}>ลงทะเบียน</button>
-					<button className={styles.bannerButton}>
+					<button
+						className={`${styles.bannerButton} text-[7px] sm:text-xs md:text-base md:px-3 md:rounded-xl lg:py-[6px] px-[6px] sm:px-2 sm:py-1 py-[2.5px] rounded-lg`}
+					>
+						ลงทะเบียน
+					</button>
+					<button
+						className={`${styles.bannerButton} text-[7px] sm:text-xs md:text-base md:px-3 md:rounded-xl lg:py-[6px] px-[6px] sm:px-2 sm:py-1 py-[2.5px] rounded-lg`}
+					>
 						ค้นหาโรงพยาบาลใกล้บ้าน
 					</button>
 				</div>
