31a7bc95ba6e75f8b30254a2489d63a6e43a8f4b
Lolymoly <teetat.wis@gmail.com>
2023-10-20 02:12:40 +0700

    finish assignment #10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,31 @@ import MenuBar from "@/components/MenuBar/page";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import NextAuthProvider from "@/providers/NextAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "ระบบจองวัคซีน",
-	description: "Vaccine Booking System",
+  title: "ระบบจองวัคซีน",
+  description: "Vaccine Booking System",
 };
 
-export default function RootLayout({
-	children,
+export default async function RootLayout({
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<MenuBar />
-				{children}
-			</body>
-		</html>
-	);
+  const session = await getServerSession(authOptions);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NextAuthProvider session={session}>
+          <MenuBar />
+          {children}
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
 }
